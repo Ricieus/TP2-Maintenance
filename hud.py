3652524431084a7852865d6feee2a29837010cce
Jimmy Tran <2247081@bdeb.qc.ca>
2024-11-29 13:32:42 -0500

    Fin de la tâche A8

--- hud.py
+++ hud.py
@@ -1,3 +1,5 @@
+import threading
+import time
 import pygame
 
 from game_settings import GameSettings, Files
@@ -20,7 +22,7 @@ class HUD:
         if not hasattr(self, '_initialized'):
             self._settings = GameSettings()
 
-            self._money_font = pygame.font.Font("fonts/boombox2.ttf", 24)
+            self._text_font = pygame.font.Font("fonts/boombox2.ttf", 24)
 
             self._bank_money = 0
             self._bank_money_surface = self._render_bank_money_surface()
@@ -33,6 +35,12 @@ class HUD:
             self._lives = self._settings.NB_PLAYER_LIVES
             self._lives_icon = pygame.image.load(HUD._LIVES_ICONS_FILENAME).convert_alpha()
             self._lives_pos= pygame.Vector2(20, self._settings.SCREEN_HEIGHT - (self._lives_icon.get_height() + 40))
+
+            self._current_pad = None
+            self._current_pad_surface = None
+            self._text_thread = None
+            self._lock = threading.Lock()
+            self._opacity = 0
 
             self.visible = False
 
@@ -48,6 +56,12 @@ class HUD:
         x = self._settings.SCREEN_WIDTH - self._trip_money_surface.get_width() - 20
         y = self._settings.SCREEN_HEIGHT - self._trip_money_surface.get_height() - 10
         screen.blit(self._trip_money_surface, (x, y))
+
+        if self._current_pad_surface:
+            self._current_pad_surface.set_alpha(self._opacity)
+            x = (self._settings.SCREEN_WIDTH - self._current_pad_surface.get_width()) / 2
+            y = self._settings.SCREEN_HEIGHT / 2
+            screen.blit(self._current_pad_surface, (x, y))
 
     def add_bank_money(self, amount: float) -> None:
         self._bank_money += round(amount, 2)
@@ -71,10 +85,34 @@ class HUD:
             self._trip_money = trip_money
             self._trip_money_surface = self._render_trip_money_surface()
 
+    def set_current_pad(self, pad: str) -> None:
+        with self._lock:
+            self._current_pad = pad
+            self._current_pad_surface = self._render_current_pad_surface()
+            self._text_thread = threading.Thread(target=self._animate_text)
+            self._text_thread.start()
+
     def _render_bank_money_surface(self) -> pygame.Surface:
         money_str = f"{self._bank_money:.2f}"
-        return self._money_font.render(f"${money_str: >8}", True, (51, 51, 51))
+        return self._text_font.render(f"${money_str: >8}", True, (51, 51, 51))
 
     def _render_trip_money_surface(self) -> pygame.Surface:
         money_str = f"{self._trip_money:.2f}"
-        return self._money_font.render(f"${money_str: >5}", True, (51, 51, 51))
+        return self._text_font.render(f"${money_str: >5}", True, (51, 51, 51))
+
+    def _render_current_pad_surface(self) -> pygame.Surface:
+        message_str = f"PAD {self._current_pad} PLEASE" if self._current_pad != "UP" else f"{self._current_pad} PLEASE"
+        return self._text_font.render(f"{message_str}", True, (255, 255, 255))
+
+    def _animate_text(self) -> None:
+        for alpha in range(0, 256, 10):
+            self._opacity = alpha
+            time.sleep(0.01)
+
+        time.sleep(1.75)
+
+        for alpha in range(255, -1, -5):
+            self._opacity = alpha
+            time.sleep(0.01)
+
+        self._animating = False
