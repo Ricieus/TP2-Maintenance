f61c5696cfa63afbfd95953a551fc2499adf39be
Marc Saliba <2235899@bdeb.qc.ca>
2024-12-03 13:43:45 -0500

    fixé bogue spawn astronaute

--- level_scene.py
+++ level_scene.py
@@ -51,11 +51,11 @@ class LevelScene(Scene):
         self._is_first_update_valid = False
 
         try:
-            config = configparser.ConfigParser()
-            config.read(GameSettings.FILE_NAMES[Files.CFG_LEVEL].replace("#", str(self._level)))
+            self.config = configparser.ConfigParser()
+            self.config.read(GameSettings.FILE_NAMES[Files.CFG_LEVEL].replace("#", str(self._level)))
 
-            self._surface = pygame.image.load(config.get("level", "surface")).convert_alpha()
-            self._music = pygame.mixer.Sound(config.get("level", "music"))
+            self._surface = pygame.image.load(self.config.get("level", "surface")).convert_alpha()
+            self._music = pygame.mixer.Sound(self.config.get("level", "music"))
 
             self._gate = Gate(GameSettings.FILE_NAMES[Files.IMG_GATE], (582, 3))
 
@@ -64,7 +64,7 @@ class LevelScene(Scene):
 
             self._taxi = Taxi((self._settings.SCREEN_WIDTH / 2, self._settings.SCREEN_HEIGHT / 2))
 
-            gate_path = config.get("gate", "gate")
+            gate_path = self.config.get("gate", "gate")
 
             gate_data = gate_path.split(",")
 
@@ -80,22 +80,22 @@ class LevelScene(Scene):
 
 
             self._obstacles = []
-            for key in config["obstacles"]:
-                img_path, x, y = config.get("obstacles", key).split(", ")
+            for key in self.config["obstacles"]:
+                img_path, x, y = self.config.get("obstacles", key).split(", ")
                 self._obstacles.append(Obstacle(img_path, (int(x), int(y))))
             self._obstacle_sprites = pygame.sprite.Group()
             self._obstacle_sprites.add(self._obstacles)
 
             self._pumps = []
-            for key in config["pumps"]:
-                img_path, x, y = config.get("pumps", key).split(", ")
+            for key in self.config["pumps"]:
+                img_path, x, y = self.config.get("pumps", key).split(", ")
                 self._pumps.append(Pump(img_path, (int(x), int(y))))
             self._pump_sprites = pygame.sprite.Group()
             self._pump_sprites.add(self._pumps)
 
             self._pads = []
-            for key in config["pads"]:
-                img_path, x, y, width, height = config.get("pads", key).split(", ")
+            for key in self.config["pads"]:
+                img_path, x, y, width, height = self.config.get("pads", key).split(", ")
                 self._pads.append(Pad(int(key[3:]), img_path, (int(x), int(y)), int(width), int(height)))
             self._pad_sprites = pygame.sprite.Group()
             self._pad_sprites.add(self._pads)
@@ -109,14 +109,16 @@ class LevelScene(Scene):
             fatal_error_app = FatalError()
             fatal_error_app.run(filename)
 
-    def spawn_astronaut(self, start_pad, end_pad):
+    def _spawn_astronaut(self, start_pad_number, end_pad_number):
         """ Crée un astronaute à partir d'un pad de départ et d'arrivée. """
-        astronaut = Astronaut(start_pad, end_pad)
-        self._astronauts.append(astronaut)
-        print(self._astronauts.__len__())
-        return astronaut
+        start_pad = self._pads[int(start_pad_number) - 1]
+        try:
+            end_pad = self._pads[int(end_pad_number) - 1]
+        except ValueError:
+            end_pad = Pad.UP
+        return Astronaut(start_pad, end_pad)
 
-    def jingle_sound_play(self):
+    def _jingle_sound_play(self):
         self._is_jingle_sound_on = True
         self._jingle_begin_time = pygame.time.get_ticks()
         self._jingle_sound_effect.play()
@@ -132,7 +134,7 @@ class LevelScene(Scene):
             if event.key == pygame.K_SPACE and self._taxi.is_destroyed():
                 self._taxi.reset()
                 self._retry_current_astronaut()
-                self.jingle_sound_play()
+                self._jingle_sound_play()
                 return
 
         if self._taxi:
@@ -144,7 +146,7 @@ class LevelScene(Scene):
         :param delta_time: temps écoulé (en secondes) depuis la dernière trame affichée
         """
         if not self._is_first_update_valid: #Condition pour voir si le update est fais une fois
-            self.jingle_sound_play()
+            self._jingle_sound_play()
 
             self._is_first_update_valid = True
             return
@@ -204,9 +206,8 @@ class LevelScene(Scene):
                 self._astronaut.wait()
         else:
             if self._nb_taxied_astronauts < len(self._astronauts) and time.time() - self._last_taxied_astronaut_time >= LevelScene._TIME_BETWEEN_ASTRONAUTS:
-                self._astronaut = self._astronauts[self._nb_taxied_astronauts]
-                new_astronaut = self.spawn_astronaut(self._pads[0], self._pads[3])
-                self._astronaut = new_astronaut
+                astronaut_info = self._astronauts[self._nb_taxied_astronauts]
+                self._astronaut = self._spawn_astronaut(astronaut_info[0], astronaut_info[1])
                 self._last_taxied_astronaut_time = time.time()
 
         # Mise à jour du taxi et gestion des collisions
@@ -265,12 +266,10 @@ class LevelScene(Scene):
     def _retry_current_astronaut(self) -> None:
         """ Replace le niveau dans l'état où il était avant la course actuelle. """
         self._gate.close()
-        self._astronauts = [Astronaut(self._pads[3], self._pads[0]),
-                            Astronaut(self._pads[2], self._pads[4]),
-                            Astronaut(self._pads[0], self._pads[1]),
-                            Astronaut(self._pads[4], self._pads[2]),
-                            Astronaut(self._pads[1], self._pads[3]),
-                            Astronaut(self._pads[0], Pad.UP)]
+        self._astronauts = []
+        for key in self.config["astronauts"]:
+            astronaut = self.config.get("astronauts", key).split(", ")
+            self._astronauts.append(astronaut)
         self._last_taxied_astronaut_time = time.time()
         self._astronaut = None
 
